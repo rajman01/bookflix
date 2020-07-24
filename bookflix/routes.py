77b8033b9a7ab6e2a0e61a8f122c7cf8fa3abc85
alameen <alameenraji31@gmail.com>
2020-07-24 04:32:22 +0100

    wip

--- bookflix/routes.py
+++ bookflix/routes.py
@@ -278,16 +278,16 @@ def api(isbn):
 					"review_count": rating[1], "average_score": rating[0]})
 
 
-@app.errorhandler(404)
-def error_404(error):
-	return render_template('404.html'), 404
-
-
-@app.errorhandler(403)
-def error_403(error):
-	return render_template('403.html'), 403
-
-
-@app.errorhandler(500)
-def error_500(error):
-	return render_template('500.html'), 500
+# @app.errorhandler(404)
+# def error_404(error):
+# 	return render_template('404.html'), 404
+#
+#
+# @app.errorhandler(403)
+# def error_403(error):
+# 	return render_template('403.html'), 403
+#
+#
+# @app.errorhandler(500)
+# def error_500(error):
+# 	return render_template('500.html'), 500
